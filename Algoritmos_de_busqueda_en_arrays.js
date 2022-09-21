61bcebc3241e8c5083fbe5efabe9b8c4a38d5f10
Facundo Rocha <67356139+FacundoRocha18@users.noreply.github.com>
2022-09-21 16:46:37 -0300

    Added SelectionSort

--- Algoritmos_de_busqueda_en_arrays.js
+++ Algoritmos_de_busqueda_en_arrays.js
@@ -97,6 +97,28 @@ const duplicates2 = (array) => {
 //console.log(duplicates2(array_con_duplicados));
 //console.log(duplicates2(array_sin_duplicados));
 
+const array = [3, 5, 2, 9, 6];
+
+const SelectionSort = (array) => {
+  for (let i = 0; i < array.lenght; i++) {
+    let indiceMenor = i;
+    for (let j = i + 1; j < array.lenght; j++) {
+      if (array[j] < array[indiceMenor]) {
+        indiceMenor = j;
+      }
+    }
+    if (indiceMenor !== i) {
+      let temp = array[i];
+      array[i] = array[indiceMenor];
+      array[indiceMenor] = temp;
+    }
+  }
+  return array;
+};
+
+console.log(SelectionSort(array));
+
+
 // Complejidad y eficiencia (pasos a seguir) para Array de 9 elementos: 
 // Complejidad con LinearSearch: 9
 // Complejidad con BinarySearch: 4
