4109317113b1e3dd8a883bfec9f431f8966a3d73
Facundo Rocha <facundo.rocha@outlook.com>
2022-09-24 16:05:45 -0300

    Added InsertionSort

--- Algorithms/InsertionSort.js
+++ Algorithms/InsertionSort.js
@@ -31,36 +31,3 @@ const InsertionSort_original = (arreglo) => {
 console.log(array1)
 InsertionSort_original(array1)
 console.log(array1)
-
-const InsertionSort_modificado = (arreglo) => {
-
-  const size = arreglo.length;
-  let comparaciones = 0;
-  let intercambios = 0;
-  let i = 1;
-
-  while ( i < size) {
-    const key = arreglo[i];
-    let j = i - 1;
-
-    // Compare key with each element on the left of it until an element smaller than
-    // it is found.
-    // For descending order, change key < array[j] to key > array[j].
-    while (j >= 0 && key < arreglo[j]) {
-      arreglo[j + 1] = arreglo[j];
-      comparaciones++;
-      j--;
-    }
-
-    // Place key at after the element just smaller than it.
-    arreglo[j + 1] = key;
-    intercambios++;
-  }
-  console.log('Comparaciones realizadas: ' + comparaciones);
-  console.log('Intercambios realizados: ' + intercambios);
-}
-console.log(array2)
-InsertionSort_modificado(array2)
-console.log(array2)
-
-
